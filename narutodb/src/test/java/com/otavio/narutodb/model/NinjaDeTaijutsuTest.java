2bff4070c3576091cebb06798e152dbd57ec9359
OtavioRdR <otaviorosa.rdr@gmail.com>
2025-01-15 16:38:08 -0300

    arruamndo testes junit

--- narutodb/src/test/java/com/otavio/narutodb/model/NinjaDeTaijutsuTest.java
+++ narutodb/src/test/java/com/otavio/narutodb/model/NinjaDeTaijutsuTest.java
@@ -8,15 +8,12 @@ public class NinjaDeTaijutsuTest {
     @Test
     public void testUsarJutsu() {
         NinjaDeTaijutsu ninja = new NinjaDeTaijutsu("Naruto", 17, "Konoha", 100);
-        // Aqui você pode verificar se o método usarJutsu funciona corretamente.
-        // Para isso, você pode refatorar o método para retornar uma String em vez de imprimir.
     }
 
     @Test
     public void testAdicionarJutsu() {
         NinjaDeTaijutsu ninja = new NinjaDeTaijutsu("Naruto", 17, "Konoha", 100);
         ninja.adicionarJutsu("Rasengan");
-        // Verifica se o jutsu foi adicionado
         assertTrue(ninja.getJutsus().contains("Rasengan"));
     }
 
@@ -24,7 +21,6 @@ public class NinjaDeTaijutsuTest {
     public void testAumentarChakra() {
         NinjaDeTaijutsu ninja = new NinjaDeTaijutsu("Naruto", 17, "Konoha", 100);
         ninja.adicaoAoChakra(20);
-        // Verifica se o chakra foi aumentado corretamente
         assertEquals(120, ninja.getChakra());
     }
 }
